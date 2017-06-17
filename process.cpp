89627f86ba05cbda2709abe12e035e5ff436977e
ypaafw <ypaafw@gmail.com>
2017-06-17 14:54:43 +0800

    main file

--- process.cpp
+++ process.cpp
@@ -43,101 +43,7 @@ int main(){
 	 	imwrite("content.bmp", showimg);*/
 	 	//waitKey();
 		Mat ROI_left = bw2(Range(y, y + height), Range(x, x + width)).clone();
-		//--------------对直线进行分析，获取轮廓最小外界矩形的旋转角度，中心-------------------------------
-	/*	vector<vector<Point>> contours;
-		vector<Vec4i> hierarchy;
-		findContours(ROI_left, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-		vector<vector<Point>> cRegion;
-		vector<Point2f> cCenter;
-		vector<RotatedRect>cRect;
-		vector<bool> selected;
-		for (size_t i = 0; i < contours.size(); i++){
-			for (size_t j = 0; j < contours[i].size(); j++)
-			{
-				contours[i][j] += Point(x, y);
-			}
-			vector<Point> single = contours[i];
-			double area0 = contourArea(single);
-			Point2f center;
-			
-			center.x = moments(single, true).m10 / moments(single, true).m00;
-			center.y = moments(single, true).m01 / moments(single, true).m00;
-			RotatedRect rect = minAreaRect(contours[i]);
-			if (rect.size.width < rect.size.height){
-				rect.angle = 90 + rect.angle;
-				float tmp = rect.size.width;
-				rect.size.width = rect.size.height;
-				rect.size.height = tmp;
-			}
-			if (area0 > 200){
-				char c[100];
-				sprintf_s(c, "(%0.0f,%0.0f)", center.x, center.y);
-				putText(showimg, c, center - Point2f(20, 20), FONT_HERSHEY_SIMPLEX, 0.8, cvScalar(255, 0, 255), 2);
-				cRegion.push_back(contours[i]);
-				cCenter.push_back(center);
-				cRect.push_back(rect);
-				selected.push_back(true);
-				drawContours(showimg, cRegion, -1, Scalar(0, 0, 220), 1);
-			}
-		}
-		//-------------筛选区域，只取同一条直线阈值化之后最大的一部分----------------
-		for (size_t i = 0; i < cCenter.size() - 1; i++){
-			for (size_t j = i + 1; j < cCenter.size(); j++){
-				if (abs(cCenter[i].y - cCenter[j].y) <= 5){
-					if (cRect[i].size.width > cRect[j].size.width){
-						selected[i] = true;
-						selected[j] = false;
-					}
-					else{
-						selected[j] = true;
-						selected[i] = false;
-					}
-				}
-			}
-		}	 
-		for (size_t i = 0; i < selected.size(); i++){
-			Information tmp;
-			if (selected[i]){
-				tmp.center = cCenter[i];
-				tmp.contour = cRegion[i];
-				tmp.rect = cRect[i];
-				line_result.push_back(tmp);
-			}
-		}
-		for (int i = 0; i < line_result.size() - 1; i++){
-			for (int j = i + 1; j < line_result.size(); j++){
-				Information tmp;
-				if (line_result[i].center.y > line_result[j].center.y){
-					tmp = line_result[i];
-					line_result[i] = line_result[j];
-					line_result[j] = tmp;
-				}
-			}
-		} 		
-		cout << "旋转角度与位置" << endl;
-		line_result[0].rect.angle = (line_result[0].rect.angle + line_result[1].rect.angle + line_result[2].rect.angle)/3;
-		//line_result[0].center.y = 51;
-		line_result[1].rect.angle = line_result[0].rect.angle;
-		//line_result[1].center.y = 84;
-		line_result[2].rect.angle = line_result[0].rect.angle;
-		for (int i = 0; i < line_result.size(); i++){
-			Information tmp = line_result[i];
-			float k = tanh((float)(tmp.rect.angle) / 180 * 3.1415926);
-			float b = tmp.center.y - k*(float)tmp.center.x;
-			Point start;
-			start.x = 0;
-			start.y = b;
-			Point end;
-			end.x = bw2.cols;
-			end.y = end.x*k + b;
-			line_result[i].lp = Vec2f(k, b);
-			cout << line_result[i].center << ", width = " << line_result[i].rect.size.width
-				<< ", height = " << line_result[i].rect.size.height << ", angle = " <<
-				line_result[i].rect.angle << " ,直线方程为： y = " << k << " * x + " <<
-				b << endl;
-		 	line(showimg, start, end, Scalar(0, 255, 0), 1);
-		}
-		*/
+		 
 		vector<Information> line_result;
 		////-------------------------------------拟合直线，提取轮廓-------------------------------------
 		lightlines(ROI_left, line_result, 3, 3, showimg, true);
@@ -149,21 +55,19 @@ int main(){
 			float dminus = 0;
 			for (size_t j = 0; j < contour.size(); j++){
 				Point t = contour[j];
-				/*if (t.x != x + 1 && t.x != x && t.x != x + width - 1 && t.x != x + width - 2)*/{
-					float distance = distance_point2lines(t, line_result[i].lp);
-					char c[100];
-					sprintf_s(c, "%0.0f", (distance));
-					double frontscale = 0.25;
-					if (j%2 == 0){
-						dis_minus = Point3f(t.x, t.y,  (distance));
-						dminus += distance;
-						line_result[i].point_distance_minus.push_back(dis_minus);						
-					}
-					else{
-						dis_plus = Point3f(t.x, t.y, (distance));
-						dplus += distance;
-						line_result[i].point_distance_plus.push_back(dis_plus);						
-					}
+				float distance = distance_point2lines(t, line_result[i].lp);
+				char c[100];
+				sprintf_s(c, "%0.0f", (distance));
+				double frontscale = 0.25;
+				if (j % 2 == 0){
+					dis_minus = Point3f(t.x, t.y, (distance));
+					dminus += distance;
+					line_result[i].point_distance_minus.push_back(dis_minus);
+				}
+				else{
+					dis_plus = Point3f(t.x, t.y, (distance));
+					dplus += distance;
+					line_result[i].point_distance_plus.push_back(dis_plus);
 				}
 			}
 			line_result[i].average_distance_minus = float(dminus / width);
